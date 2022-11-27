67bce71538ecd271511d28c84d55be9a5bb65e5a
Gruzin <grzegorz.jagielski.2019@zsa.pwr.edu.pl>
2022-11-27 18:54:31 +0100

    Modifying .gitignore

---
+++ conf.py
@@ -0,0 +1,63 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Typesetting Guide'
+copyright = '2022, ModerNews'
+author = 'ModerNews'
+
+# The full version, including alpha/beta/rc tags
+release = '1a'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.duration',
+              'sphinx.ext.doctest',
+              'sphinx.ext.autodoc',
+              'sphinx_toolbox.collapse',
+              'sphinxawesome_theme',
+              ]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme = "sphinxawesome_theme"
+html_static_path = ['_static']
+
+autodoc_preserve_defaults = True
+
+autodoc_pydantic_model_show_config_summary = False
+autodoc_pydantic_model_summary_list_order = 'bysource'
+autodoc_pydantic_model_hide_paramlist = True
+autodoc_pydantic_model_signature_prefix = 'class'
+autodoc_pydantic_settings_members = False
+autodoc_pydantic_model_undoc_members = False
+
+autodoc_typehints = "none"
